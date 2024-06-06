03edbaa502a50908b0fbfaa31453e67e55399f32
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-06-07 00:00:40 +0530

    Create outputs.tf

---
+++ fold1/outputs.tf
@@ -0,0 +1,23 @@
+output "provpc" {
+  value = aws_vpc.provpc.id
+}
+
+output "proig" {
+  value = aws_internet_gateway.proig.id
+}
+
+output "aval_1a_subnet" {
+  value = aws_subnet.aval_1a_subnet.id
+}
+
+output "aval_1b_subnet" {
+  value = aws_subnet.aval_1b_subnet.id
+}
+
+output "aval_1c_subnet" {
+  value = aws_subnet.aval_1c_subnet.id
+}
+
+output "first_instance_ami" {
+  value = aws_ami_from_instance.first_instance_ami.id
+}
